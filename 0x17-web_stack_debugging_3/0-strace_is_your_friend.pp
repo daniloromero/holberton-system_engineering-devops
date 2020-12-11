5de89371a54ee3d814bcc1eb78e71c708bf92841
daniloromero <1601@holbertonschol.com>
2020-12-10 21:49:54 -0500

    Remove directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Debug Apache server, typo in executable filename
-exec { 'debuggin_3':
-  path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-  command => 'sed -i "s/phpp/php/1" /var/www/html/wp-settings.php',
-}
-
