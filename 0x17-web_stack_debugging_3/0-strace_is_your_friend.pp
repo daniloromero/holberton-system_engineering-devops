e7c3ab3a3c51b66c389ee9bf5ee7e9f230183259
danilormero <danilo@DESKTOP-L8K7EJB.localdomain>
2021-02-18 21:13:30 -0500

    Puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Webdebugging_3
 exec { 'Typo correction':
   path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
     }
