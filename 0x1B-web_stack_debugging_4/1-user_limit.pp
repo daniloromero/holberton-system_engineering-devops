927c4dd2c638354dc19bd7cc49baec5799c84940
daniloromero <danilo@DESKTOP-L8K7EJB.localdomain>
2021-02-25 19:50:36 -0500

    Removed extra closing bracket

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 exec { 'fix--for-linux-secuity_limits':
   environment => ['DIR=/etc/security/limits.conf',
                   'OLD1=nofile 5',
-                  'NEW1=nofile 15000'],
+                  'NEW1=nofile 15000',
                   'OLD2=nofile 4',
                   'NEW2=nofile 15000'],
   command     => 'sudo sed -i "s/$OLD1/$NEW1/" $DIR; sudo sed -i "s/$OLD2/$NEW2/" $DIR';
